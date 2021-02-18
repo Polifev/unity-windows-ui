ff62b7da1442bb14908dd5a787dfd123b5d50f24
Polifev <pol007@skynet.be>
2021-02-18 11:07:44 +0100

    Start tracking

---
+++ Assets/WindowUI/Scripts/Window.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class Window : MonoBehaviour
+{
+	#region Constants
+	private static readonly int TITLE_BAR_SIZE = 20;
+	#endregion
+
+	#region Attributes
+	// Parameters
+	[SerializeField]
+	private bool Draggable = true;
+	[SerializeField]
+	private bool Closable = true;
+	[SerializeField]
+	private string Title = "New window";
+
+	// Components
+	public BoxCollider2D Collision { get; private set; }
+	private RectTransform _area;
+	private Text _titleText;
+	private Button _crossButton;
+
+	// Util
+	private bool _isDragged = false;
+	private Vector2 _relativeDraggingPosition = new Vector2(0, 0);
+	private ISet<IWindowEventListener> listeners = new HashSet<IWindowEventListener>();
+	#endregion
+
+	#region Overridden methods
+	void Start()
+    {
+		//InitComponents();
+		//SetTitle(_title);
+		//SetClosable(_closable);
+		//SetCollisionDimensions();
+		//InitCrossButton();
+	}
+
+	private void OnValidate()
+	{
+		InitComponents();
+		SetTitle(Title);
+		SetClosable(Closable);
+		SetCollisionDimensions();
+		// Place here all things that must happen when you change
+		// a value in the editor
+	}
+
+	void Update()
+    {
+		if (Draggable)
+		{
+			UpdatePosition();
+		}
+	}
+	#endregion
+
+	#region Public methods
+	public void Init()
+	{
+		InitComponents();
+		SetTitle(Title);
+		SetClosable(Closable);
+		SetCollisionDimensions();
+		InitCrossButton();
+	}
+
+	/// <summary>
+	/// Set the title of the window
+	/// </summary>
+	/// <param name="title">The new title</param>
+	public void SetTitle(string title)
+	{
+		Title = title;
+		GetComponentInChildren<Text>().text = Title;
+	}
+
+	/// <summary>
+	/// Set whether the window should be closable by using the cross button
+	/// </summary>
+	/// <param name="closable">new value for argument closable</param>
+	public void SetClosable(bool closable)
+	{
+		Closable = closable;
+		_crossButton.interactable = closable;
+	}
+
+	/// <summary>
+	/// Close the window
+	/// </summary>
+	public void CloseWindow()
+	{
+		WindowEventArgs args = new WindowEventArgs();
+		args.Target = this;
+
+		foreach (var listener in listeners)
+			listener.WindowClosed(args);
+		Destroy(gameObject);
+	}
+
+	public void Move(Vector2 movement)
+	{
+		transform.position += new Vector3(movement.x, movement.y, 0);
+	}
+
+	public void BeginDrag(Vector2 position)
+	{
+		_isDragged = true;
+		_relativeDraggingPosition = Input.mousePosition;
+		transform.SetAsLastSibling();
+	}
+
+	public void EndDrag()
+	{
+		_isDragged = false;
+	}
+
+	public void AddListener(IWindowEventListener listener)
+	{
+		listeners.Add(listener);
+	}
+
+	public void RemoveListener(IWindowEventListener listener)
+	{
+		listeners.Remove(listener);
+	}
+	#endregion
+
+	#region Private methods
+	/// <summary>
+	/// Gather all the necessary components from the object
+	/// </summary>
+	private void InitComponents()
+	{
+		Collision = GetComponent<BoxCollider2D>();
+		_area = GetComponent<RectTransform>();
+
+		// TODO check if always correct
+		_titleText = GetComponentInChildren<Text>();
+		_crossButton = GetComponentInChildren<Button>();
+	}
+
+	/// <summary>
+	/// Set the collision box's boundaries to be the same as rect transform
+	/// </summary>
+	private void SetCollisionDimensions()
+	{
+		Vector2 dimensions = _area.sizeDelta;
+		dimensions.y = TITLE_BAR_SIZE;
+		Collision.size = dimensions;
+		Collision.offset = new Vector2(dimensions.x, -dimensions.y) / 2;
+	}
+
+	/// <summary>
+	/// Set the action to be done when cross button is pressed
+	/// </summary>
+	private void InitCrossButton()
+	{
+		_crossButton.onClick.AddListener(CloseWindow);
+	}
+
+	/// <summary>
+	/// Make the window follow the mouse
+	/// </summary>
+	private void UpdatePosition()
+	{
+		if (_isDragged)
+		{
+			Vector2 newPosition = Input.mousePosition;
+			Rect screen = new Rect(new Vector2(TITLE_BAR_SIZE, TITLE_BAR_SIZE), new Vector2(Screen.width - (2*TITLE_BAR_SIZE), Screen.height - (2*TITLE_BAR_SIZE)));
+			if (screen.Contains(newPosition))
+			{
+				Vector2 movement = newPosition - _relativeDraggingPosition;
+				_relativeDraggingPosition = newPosition;
+				Move(movement);
+			}
+		}
+	}
+	#endregion
+}
+
+public interface IWindowEventListener
+{
+	void WindowClosed(WindowEventArgs args);
+}
+
+public struct WindowEventArgs
+{
+	public Window Target { get; set; }
+}
---
+++ Assets/WindowUI/Scripts/WindowsManager.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowsManager : MonoBehaviour, IWindowEventListener
+{
+	#region Constants
+	private static readonly int OFFSET = 30;
+	#endregion
+
+	#region Attributes
+	public static WindowsManager Instance { get; private set; }
+
+	[SerializeField]
+	private List<WindowTypeElement> _windowPrefabsList = new List<WindowTypeElement>();
+	private IDictionary<string, GameObject> _windowPrefabsDict = new Dictionary<string, GameObject>();
+	private ISet<Window> _windows = new HashSet<Window>();
+	private Vector2 lastSpawnPos = new Vector2(0, Screen.height);
+	#endregion
+
+	#region Overridden methods
+	// Start is called before the first frame update
+	void Start()
+	{
+		Instance = this;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		const int MOUSE_LEFT = 0;
+		Vector2 mousePosition = Input.mousePosition;
+		if (Input.GetMouseButtonDown(MOUSE_LEFT))
+		{
+			int maxSIndex = -1;
+			Window selected = null;
+			foreach (Window win in _windows)
+			{
+				if (win.Collision.OverlapPoint(mousePosition))
+				{
+					int sIndex = win.transform.GetSiblingIndex();
+					if (sIndex > maxSIndex)
+					{
+						selected = win;
+						maxSIndex = sIndex;
+					}
+				}
+			}
+			if (selected != null)
+			{
+				selected.BeginDrag(mousePosition);
+			}
+		}
+		if (Input.GetMouseButtonUp(MOUSE_LEFT))
+		{
+			foreach (Window win in _windows)
+			{
+				win.EndDrag();
+			}
+		}
+	}
+
+	private void OnValidate()
+	{
+		// Fill the dictionary
+		_windowPrefabsDict.Clear();
+		foreach (var type in _windowPrefabsList)
+		{
+			// Doublons are ignored
+			if(! _windowPrefabsDict.ContainsKey(type.Name))
+				_windowPrefabsDict.Add(type.Name, type.Prefab);
+		}
+	}
+	#endregion
+
+	#region Public methods
+	public GameObject CreateWindow(string title, string type)
+	{
+		GameObject windowObject = Instantiate(_windowPrefabsDict[type]);
+		Window window = windowObject.GetComponent<Window>();
+		window.Init();
+		window.Move(lastSpawnPos);
+		lastSpawnPos += new Vector2(OFFSET, -OFFSET);
+		if (lastSpawnPos.x > Screen.width - 400 || lastSpawnPos.y < 400)
+			lastSpawnPos = new Vector2(0, 0);
+		window.SetTitle(title);
+		windowObject.transform.SetParent(transform);
+
+		// Handling windows listing
+		_windows.Add(window);
+		window.AddListener(this);
+
+		return windowObject;
+	}
+
+	public void WindowClosed(WindowEventArgs args)
+	{
+		_windows.Remove(args.Target);
+	}
+	#endregion
+}
+
+[System.Serializable]
+public struct WindowTypeElement{
+	public string Name;
+	public GameObject Prefab;
+}
